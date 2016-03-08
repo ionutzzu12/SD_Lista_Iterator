a93dcc6f4ba63749ccbd26db995268882d582a6b
ioanachirca <jo.chirca@gmail.com>
2016-03-08 23:35:17 +0200

    list.h : adaugat metoda insert + template + sters initializare, nu asa functioneaza main.cpp: testat metode

--- list.h
+++ list.h
@@ -4,44 +4,60 @@ using namespace std;
 
 #ifndef __RESIZABLE_ARRAY__
 #define __RESIZABLE_ARRAY__
- 
-#define MAX_SIZE 100 // o dimensiune maxima prestabilita
- 
+
+
+template<typename T>
 struct ResizableArray {
-	int *array;
+	T *array;
 	int size; //se va modifica la fiecare remove/add de element!
- 
+
 	// Metoda de initializare
-	ResizableArray(int size = 0) {
-		this->size = size;
-		this->array = new int[ size ];
+	ResizableArray() {
+		this->size = 0;
+		this->array = new T[ 0 ];
 	}
-	
+
+	// Adauga elem pe pozitia index
+	void insert (T element, int index){
+		T *tmpArray = new T[size+1];
+		int i;
+
+		for(i =0; i < index; i++)
+			tmpArray[i] = array[i];
+		tmpArray[index]= element;
+		for(i = index + 1; i< size + 1; i++)
+			tmpArray[i] = array[i-1];
+
+		delete [] array;
+		array = tmpArray;
+		size = size + 1;
+	}
+
 	// Metoda de eliminare a unui element la index dat
 	void remove(int index) {
 		int i;
 		for(i = index; i < size-1; i++)
 			array[i] = array[i+1];
-			
-		int *tmpArray = new int[size-1];
+
+		T *tmpArray = new T[size-1];
 		for(i = 0; i < size-1; i++)
 			tmpArray[i] = array[i];
-		
+
 		delete[] array;
-		
+
 		array = tmpArray;
 		size = size-1;
-		
+
 	}
 
 	// Metoda care afiseaza elementele vectorului.
 	void print() {
 		std::cout << "Continut: ";
-		for ( int i = 0; i < size; ++i ) {
+		for ( int i = 0; i < size; i++ ) {
 			std::cout << array[i] << ' ';
 		}
 		std::cout << '\n';
 	}
 };
- 
+
 #endif // __RESIZABLE_ARRAY__
